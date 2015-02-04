acce5c5e82fbc5c8a154b50aa44267a7a6d26f95
Gordon RASS-KWASI <grass-kw@e1r11p7.42.fr>
2015-02-04 12:56:47 +0100

    push de la lib avec free_split mv en free_tab

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: grass-kw <grass-kw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/22 12:39:39 by grass-kw          #+#    #+#             */
-/*   Updated: 2015/01/29 12:26:32 by grass-kw         ###   ########.fr       */
+/*   Updated: 2015/02/04 12:47:32 by grass-kw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,6 @@
 # define LIBFT_H
 # include <string.h>
 # include <stdlib.h>
-
-typedef struct		s_list
-{
-	void			*content;
-	size_t			content_size;
-	struct s_list 	*next;
-}					t_list;
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
@@ -79,12 +72,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-void	ft_free_split(char **arg);
-t_list	*ft_lstnew(void const *content, size_t content_size);
-void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
-void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
-void	ft_lstadd(t_list **alst, t_list *new);
-void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-t_list	*ft_lstmap(t_list *lst, t_list * (*f)(t_list *elem));
+void	ft_free_tab(char **arg);
 
 #endif
